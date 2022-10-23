be6442dcb982470d70460c264dc8d9e0e1f8a2bc
dan wilcock <daniel.wilcock@devopsgroup.com>
2022-10-23 11:54:53 +0100

    testing approver check

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "~> 1.2.4"
+  required_version = "=1.3.2"
 
   required_providers {
     azurerm = {
@@ -10,4 +10,10 @@ terraform {
   }
 
   backend "azurerm" {}
+}
+
+
+resource "azurerm_resource_group" "name" {
+  name="test"
+  location="uksouth"
 }
